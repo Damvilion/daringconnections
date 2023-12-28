540b3d1fb9224a87201ac8f9c38a34b23eef94eb
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-28 01:33:03 -0500

    Not Found Component

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+    return (
+        <div className='flex flex-col items-center'>
+            <h2>Page Not Found</h2>
+            <p>Could not find requested resource</p>
+            <Link className='bg-blue-300 p-3 rounded-lg' href='/'>
+                Return Home
+            </Link>
+        </div>
+    );
+}
