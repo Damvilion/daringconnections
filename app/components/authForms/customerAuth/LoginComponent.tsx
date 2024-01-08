5a100e02359d1b5d9cda79280e454f738c0c8c16
Noel <noelohaeri@gmail.com>
2024-01-08 04:24:40 -0500

    Remove logUser function and update login form

--- app/components/authForms/customerAuth/LoginComponent.tsx
+++ app/components/authForms/customerAuth/LoginComponent.tsx
@@ -40,9 +40,6 @@ const LoginComponent = () => {
         reset();
     };
 
-    const logUser = async () => {
-        console.log(FirebaseAuth.currentUser);
-    };
     return (
         <div className='flex flex-col border-slate-900 border-solid border rounded-md shadow-md p-3 sm:p-4 md:p-7 lg:p-10'>
             <h1 className='font-bold text-purple-800 text-medium md:text-xl my-2'>Member Login</h1>
@@ -61,6 +58,8 @@ const LoginComponent = () => {
                             })}
                             className='text-sm md:text-base border-solid border border-black p-2 md:p-3 rounded-md'
                             type='text'
+                            id='email'
+                            autoComplete='email'
                         />
                         {errors.email && <span className='text-xs text-red-500'>{errors.email.message}</span>}
                     </div>
@@ -78,6 +77,7 @@ const LoginComponent = () => {
                             })}
                             className='text-sm md:text-base border-solid border border-black p-2 md:p-3 rounded-md'
                             type='password'
+                            id='password'
                         />
                         {loginError === 'Problem with email or password' && <span className='text-xs text-red-500'>{loginError}</span>}
                     </div>
@@ -88,20 +88,31 @@ const LoginComponent = () => {
                         Log in
                     </ShadButton>
                 </div>
-                <Separator className='my-2' />
-                <div className='text-xs w-full text-center'>SIGN IN WITH GOOGLE PLACEHOLDER</div>
-
+                <p className='text-xs text-gray-500 w-full -m-2 mx-auto'>or</p>
+                <Separator className='my-1' />
                 <div className='w-full'>
                     <Link href='/signup'>
-                        <ShadButton disabled={isSubmitting} className='mx-auto bg-[#f15daf] text-white shadow-lg hover:bg-[#c764d4]'>
+                        <ShadButton disabled={isSubmitting} className='bg-[#c743d8] text-white shadow-lg hover:bg-[#c764d4]'>
                             Create Free Account
                         </ShadButton>
                     </Link>
                 </div>
+
+                <div className='w-full'>
+                    <ShadButton
+                        type='button'
+                        // onClick={signINWithGoogle}
+                        // className='rounded-full border-solid border-2 border-black text-black bg-white mx-auto hover:bg-white'
+                        variant='googleButton'>
+                        Sign In With Google
+                    </ShadButton>
+                </div>
+
+                {/* <div className='text-xs w-full text-center'>SIGN IN WITH GOOGLE PLACEHOLDER</div> */}
             </form>
-            <ShadButton onClick={logUser} className='mx-auto bg-[#f15daf] text-white shadow-lg hover:bg-[#c764d4]'>
+            {/* <ShadButton onClick={logUser} className='mx-auto bg-[#f15daf] text-white shadow-lg hover:bg-[#c764d4]'>
                 Log User
-            </ShadButton>
+            </ShadButton> */}
         </div>
     );
 };
