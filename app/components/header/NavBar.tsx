e157384f9c38da13e5d05b18fdc13f0d142e1eeb
Noel Ohaeri <noelohaeri@gmail.com>
2024-01-09 18:58:15 -0500

    Remove unused logo and update image source

--- app/components/header/NavBar.tsx
+++ app/components/header/NavBar.tsx
@@ -1,12 +1,11 @@
 'use client';
 import Image from 'next/image';
 import React from 'react';
-import dLogoPng from '@/app/assets/logo/dLogoPng.png';
 
 const NavBar = () => {
     return (
         <nav className='flex items-center justify-around'>
-            <Image src={dLogoPng} alt='DaringConnections' width={100} height={100} />
+            <Image src='/assets/logo/dLogoPng.png' alt='DaringConnections' width={100} height={100} />
         </nav>
     );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import LiveKitGeneral from './components/LiveKit/LiveKitGeneral/LiveKitGeneral';
 import MainCard from '@/app/components/auth/customerCard/MainCard';
+// import Header from './components/header/Header';
 
 export default function Home() {
     return (
         <main className='h-screen relative'>
+            {/* <Header /> */}
             <MainCard />
             <div className='flex h-full items-center justify-center'>
                 <LiveKitGeneral />
