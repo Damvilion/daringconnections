c2f87a77dd81f132c289e86aac87a516d4353c59
Noel Ohaeri <noelohaeri@gmail.com>
2024-01-09 18:58:06 -0500

    Add icons and update UI in MainCard component

--- app/components/auth/customerCard/MainCard.tsx
+++ app/components/auth/customerCard/MainCard.tsx
@@ -3,12 +3,13 @@ import { Card, CardContent, CardHeader } from '@/app/components/shadCn/ui/card';
 import { Avatar, AvatarImage } from '../../shadCn/ui/avatar';
 import { Button } from '../../shadCn/ui/button';
 import { Separator } from '../../shadCn/ui/separator';
+import Image from 'next/image';
 
 const MainCard = () => {
     return (
         <Card className='absolute m-2'>
             <CardHeader className=''>
-                <div className='flex px-5'>
+                <div className='flex items-center px-5 gap-1'>
                     <Avatar>
                         <AvatarImage src='/icon_default.jpg' />
                     </Avatar>
@@ -22,12 +23,41 @@ const MainCard = () => {
                 </div>
             </CardHeader>
             <CardContent>
-                <div className='flex flex-col gap-1'>
-                    <Button className='w-full'>Home</Button>
-                    <Button className='w-full'>Messages</Button>
-                    <Button className='w-full'>Notifications</Button>
+                <div className='flex flex-col item-start gap-1'>
+                    <span className='flex items-center gap-2 hover:bg-purple-700 transition-colors p-1 rounded-lg cursor-pointer group'>
+                        <Image
+                            className='invert group-hover:invert-0'
+                            src='/assets/icons/home_icon_white.png'
+                            width={30}
+                            height={30}
+                            alt='Home Icon'
+                        />
+
+                        <span className='text-left w-full group-hover:text-white'>Home</span>
+                    </span>
+                    <span className='flex items-center gap-2 hover:bg-purple-700 transition-colors p-1 rounded-lg cursor-pointer group'>
+                        <Image
+                            className='invert group-hover:invert-0'
+                            src='/assets/icons/bell_icon_white.png'
+                            width={30}
+                            height={30}
+                            alt='Home Icon'
+                        />
+                        <span className='text-left w-full group-hover:text-white'>Notifications</span>
+                    </span>
+                    <span className='flex items-center gap-2 hover:bg-purple-700 transition-colors p-1 rounded-lg cursor-pointer group'>
+                        <Image
+                            className='invert group-hover:invert-0'
+                            src='/assets/icons/messages_icon_white.png'
+                            width={27}
+                            height={30}
+                            alt='Home Icon'
+                        />
+                        <span className='text-left w-full group-hover:text-white'>Messages</span>
+                    </span>
+
                     <Separator className='my-2' />
-                    <Button className='w-full'>Connect</Button>
+                    <Button className='w-full bg-purple-500 hover:bg-purple-400'>Connect</Button>
                 </div>
             </CardContent>
         </Card>
