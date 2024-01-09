1bffc40d5c5260d8015330bf41bb246e36b4b7f6
Noel <noelohaeri@gmail.com>
2024-01-09 16:53:42 -0500

    Update video element styling

--- app/components/LiveKit/LiveKitGeneral/LiveKitGeneralRoom.tsx
+++ app/components/LiveKit/LiveKitGeneral/LiveKitGeneralRoom.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LiveKitGeneralRoom = () => {
     return (
         <div className='w-full h-full'>
-            <video className='bg-slate-700'></video>
+            <video className='bg-slate-700 h-[400px] rounded-md'></video>
         </div>
     );
 };
