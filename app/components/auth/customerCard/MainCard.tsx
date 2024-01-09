48a7262885299b280ccf6349b82b3b81bcf9bdc6
Noel <noelohaeri@gmail.com>
2024-01-09 16:53:34 -0500

    Add @radix-ui/react-avatar package and update MainCard component

--- app/components/auth/customerCard/MainCard.tsx
+++ app/components/auth/customerCard/MainCard.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/app/components/shadCn/ui/card';
+import { Card, CardContent, CardHeader } from '@/app/components/shadCn/ui/card';
+import { Avatar, AvatarImage } from '../../shadCn/ui/avatar';
+import { Button } from '../../shadCn/ui/button';
+import { Separator } from '../../shadCn/ui/separator';
 
 const MainCard = () => {
     return (
         <Card className='absolute m-2'>
-            <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+            <CardHeader className=''>
+                <div className='flex px-5'>
+                    <Avatar>
+                        <AvatarImage src='/icon_default.jpg' />
+                    </Avatar>
+                    <div className='flex flex-col items-start'>
+                        <p className='text-xs font-bold'>Status: {'Anonymous'}</p>
+                        <div className='flex items-center gap-2'>
+                            <p className='text-xs font-bold'>DareCoins: {0}</p>
+                            <p className='text-xs cursor-pointer'>(get more)</p>
+                        </div>
+                    </div>
+                </div>
             </CardHeader>
             <CardContent>
-                <p>Card Content</p>
+                <div className='flex flex-col gap-1'>
+                    <Button className='w-full'>Home</Button>
+                    <Button className='w-full'>Messages</Button>
+                    <Button className='w-full'>Notifications</Button>
+                    <Separator className='my-2' />
+                    <Button className='w-full'>Connect</Button>
+                </div>
             </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
         </Card>
     );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 import LiveKitGeneral from './components/LiveKit/LiveKitGeneral/LiveKitGeneral';
-// import MainCard from './components/auth/customerCard/MainCard';
+import MainCard from '@/app/components/auth/customerCard/MainCard';
 
 export default function Home() {
     return (
-        <main className='h-screen'>
+        <main className='h-screen relative'>
+            <MainCard />
             <div className='flex h-full items-center justify-center'>
                 <LiveKitGeneral />
             </div>
-            {/* <MainCard /> */}
         </main>
     );
 }
