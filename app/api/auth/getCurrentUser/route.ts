0ca2087cdc0b9ee3b1dc721325f1d6e5ed166924
Noel Ohaeri <noelohaeri@gmail.com>
2024-01-11 04:55:38 -0500

    Refactor user profile retrieval in getCurrentUser route

--- app/api/auth/getCurrentUser/route.ts
+++ app/api/auth/getCurrentUser/route.ts
@@ -1,4 +1,4 @@
-// import { prisma_db } from '@/app/lib/prismadb';
+import { prisma_db } from '@/app/lib/prismadb';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
@@ -9,20 +9,13 @@ export async function POST(request: Request) {
         email?: string;
     };
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const user = await request.json();
+    const user: Profile = await request.json();
 
-    // const profile = await prisma_db.user.findUnique({
-    //     where: {
-    //         id: user.uid,
-    //     },
-    // });
-
-    const profile: Profile = {
-        id: 'fake_id',
-        username: 'fake_username',
-        dareCoins: 100,
-        email: 'fake_email@example.com',
-    };
+    const profile = await prisma_db.user.findUnique({
+        where: {
+            id: user.id,
+        },
+    });
 
     return NextResponse.json({ message: 'success', profile });
 }
