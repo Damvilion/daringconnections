bd4e9ac2725f4c50d228a1e5bc275b3f03fdacb6
Noel Ohaeri <noelohaeri@gmail.com>
2024-01-11 04:55:30 -0500

    Add support for Google sign-in in customerSignUp route

--- app/api/auth/customerSignUp/route.ts
+++ app/api/auth/customerSignUp/route.ts
@@ -8,12 +8,31 @@ interface BodyType {
     email: string;
     username: string;
     password: string;
+    photoUrl?: string;
+    uid?: string;
+    type?: string;
 }
 
 export async function POST(request: Request) {
     const body: BodyType = await request.json();
-    const { email, username, password } = body;
+    const { email, username, password, type, uid } = body;
 
+    if (type === 'google') {
+        try {
+            const existingUser = await prisma_db.user.findFirst({
+                where: {
+                    email,
+                },
+            });
+
+            if (!existingUser) {
+                await prisma_db.user.create({ data: { username, email, id: uid } });
+            }
+            return NextResponse.json({ message: 'success' });
+        } catch (error) {
+            return NextResponse.json({ message: 'FAILED', error });
+        }
+    }
     // Check if email or username already exists
     try {
         const existingUser = await prisma_db.user.findFirst({
@@ -24,16 +43,20 @@ export async function POST(request: Request) {
 
         if (existingUser) {
             if (existingUser.email === email && existingUser.username === username) {
+                console.log('Email & Username already exists');
                 return NextResponse.json({ message: 'ISSUE', type: 'Email & Username already exists' });
             } else if (existingUser.email === email) {
+                console.log('Email already exists');
                 return NextResponse.json({ message: 'ISSUE', type: 'Email already exists' });
             } else if (existingUser.username === username) {
+                console.log('Username already exists');
                 return NextResponse.json({ message: 'ISSUE', type: 'Username already exists' });
             }
         }
     } catch (error) {
-        return NextResponse.json({ message: 'FAILED' });
+        return NextResponse.json({ message: 'FAILED', error });
     }
+    // Creates User if User Signs in with Google
 
     try {
         const res = await createUserWithEmailAndPassword(FirebaseAuth, email, password);
