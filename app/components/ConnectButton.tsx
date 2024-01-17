201bc8970f951c4e0cfc35e76f2cd149633447be
Noel <noelohaeri@gmail.com>
2024-01-17 06:25:19 -0500

    Add ConnectButton component and update MainCard component
    The developer added a new component called ConnectButton and updated the MainCard component to use the ConnectButton component instead of the Button component.

---
+++ app/components/ConnectButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { current_user, jotai } from '@/app/jotai_store/store';
+import { useRouter } from 'next/navigation';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './shadCn/ui/tooltip';
+
+const ConnectButton = () => {
+    const [user] = jotai.useAtom(current_user);
+    const router = useRouter();
+
+    const handleClick = () => {
+        if (!user) {
+            router.push('/signup');
+        }
+    };
+    return (
+        <TooltipProvider>
+            <Tooltip delayDuration={250}>
+                <TooltipTrigger
+                    onClick={handleClick}
+                    className='w-full bg-purple-500 hover:bg-purple-400 text-white h-10 px-4 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'>
+                    Connect
+                    <TooltipContent side='right'>
+                        <div className='flex flex-col gap-1'>
+                            <span className='text-center text-sm'>1 on 1 video calls - direct message & more</span>
+                        </div>
+                    </TooltipContent>
+                </TooltipTrigger>
+            </Tooltip>
+        </TooltipProvider>
+    );
+};
+
+export default ConnectButton;
--- app/components/auth/customerCard/MainCard.tsx
+++ app/components/auth/customerCard/MainCard.tsx
@@ -1,31 +1,25 @@
 'use client';
 import React from 'react';
 import { Card, CardContent } from '@/app/components/shadCn/ui/card';
-import { Button } from '../../shadCn/ui/button';
+// import { Button } from '../../shadCn/ui/button';
 import { Separator } from '@/app/components/shadCn/ui/separator';
 import CustomerInfoCard from './CustomerInfoCard';
 import CustomerCardContent from './CustomerCardContent';
 
 import { jotai, current_user } from '@/app/jotai_store/store';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
+import ConnectButton from '@/app/components/ConnectButton';
 
 const MainCard = () => {
-    const router = useRouter();
     const [user] = jotai.useAtom(current_user);
-    const handleClick = () => {
-        if (!user) {
-            router.push('/signup');
-        }
-    };
+
     return (
         <Card className='mr-auto ml-auto lg:ml-2 lg:block hidden'>
             <CustomerInfoCard />
             <Separator className='my-2 w-[75%] mx-auto' />
             <CardContent>
                 {user && <CustomerCardContent />}
-                <Button onClick={handleClick} className='w-full bg-purple-500 hover:bg-purple-400'>
-                    Connect
-                </Button>
+                <ConnectButton />
             </CardContent>
         </Card>
     );
