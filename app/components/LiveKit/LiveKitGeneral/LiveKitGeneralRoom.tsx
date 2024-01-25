fbea6135832a925bc811f0685d1d3442dd0beb31
Noel Ohaeri <noelohaeri@gmail.com>
2024-01-25 08:38:14 -0500

    Update video container styling

--- app/components/LiveKit/LiveKitGeneral/LiveKitGeneralRoom.tsx
+++ app/components/LiveKit/LiveKitGeneral/LiveKitGeneralRoom.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LiveKitGeneralRoom = () => {
     return (
-        <div className='w-[90%] mt-5 mx-1 mr-auto ml-auto'>
-            <video className='bg-slate-700 h-[300px] md:h-[500px] lg:h-[600px] rounded-lg'></video>
+        <div className='w-[100%] mt-3 mx-1 mr-auto ml-auto'>
+            <video className='bg-slate-700 h-[300px] md:h-[500px] lg:h-[600px] rounded-sm'></video>
         </div>
     );
 };
