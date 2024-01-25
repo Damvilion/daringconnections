f297dab269733b2e1c3cbf79a2aeb112d66926e1
Noel Ohaeri <noelohaeri@gmail.com>
2024-01-25 02:45:10 -0500

    Add TypeOfLoginComponent

---
+++ app/components/auth/TypeOfLoginComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TypeOfLoginComponent = () => {
+    return (
+        <div>
+            <div>Login as Customer</div>
+            <div>Login as Influencer</div>
+        </div>
+    );
+};
+
+export default TypeOfLoginComponent;
