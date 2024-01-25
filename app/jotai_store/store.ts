ed58fb0a5ec013340b4887965ebf739144ab03c5
Noel Ohaeri <noelohaeri@gmail.com>
2024-01-25 02:44:55 -0500

    Add new atoms for liveKit and microphone connections

--- app/jotai_store/store.ts
+++ app/jotai_store/store.ts
@@ -5,8 +5,12 @@ import { Profile } from '@/app/lib/types/types';
 const current_user = atom<null | Profile>(null);
 const loadingUser = atom<boolean>(true);
 
+const liveKitConnection = atom<boolean>(false);
+
+const microphoneConnection = atom<boolean>(false);
+
 export const jotai = {
     useAtom,
 };
 
-export { current_user, loadingUser };
+export { current_user, loadingUser, liveKitConnection, microphoneConnection };
