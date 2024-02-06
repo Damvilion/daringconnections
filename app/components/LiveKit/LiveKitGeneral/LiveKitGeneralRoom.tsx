5758140f2ca802a9fd360219536f77fe005c6c9a
Noel Ohaeri <noelohaeri@gmail.com>
2024-02-06 09:08:19 -0500

    Add connection state and participant count to LiveKitGeneralRoom component

--- app/components/LiveKit/LiveKitGeneral/LiveKitGeneralRoom.tsx
+++ app/components/LiveKit/LiveKitGeneral/LiveKitGeneralRoom.tsx
@@ -1,23 +1,36 @@
-import { VideoTrack, useTracks } from '@livekit/components-react';
+import { VideoTrack, useConnectionState, useParticipants, useTracks } from '@livekit/components-react';
 import { Track } from 'livekit-client';
+import { Loader2 } from 'lucide-react';
 import React from 'react';
 
 const LiveKitGeneralRoom = () => {
     const videoStream = useTracks([Track.Source.Camera]);
+    const participants = useParticipants();
+    const connectionState = useConnectionState();
+
     // const audioStream = useTracks([Track.Source.Microphone]);
     return (
         <div className='w-[100%] mt-3 mx-1 mr-auto ml-auto bg-black'>
             {!videoStream[0] && (
-                <div className='flex justify-center'>
-                    <video height={720} width={1280} className='object-contain aspect-video'></video>
+                <div className='flex justify-center relative'>
+                    <video height={1080} width={1920} className='object-contain aspect-video'></video>
+                    {connectionState === 'connecting' ||
+                        ('disconnected' && (
+                            <Loader2 className='text-white w-42 h-42 m-auto absolute left-0 right-0 top-0 bottom-0 animate-spin'>Hello</Loader2>
+                        ))}
                 </div>
             )}
 
             {videoStream[0] && (
                 <div className='flex justify-center'>
-                    <VideoTrack height={720} width={1280} trackRef={videoStream[0]} className='object-contain aspect-video'></VideoTrack>
+                    <VideoTrack
+                        height={1080}
+                        width={1920}
+                        trackRef={videoStream[0]}
+                        className='max-w-full max-h-full object-contain aspect-video'></VideoTrack>
                 </div>
             )}
+            <div className='text-black bg-white text-xs text-right'>Current Viewers: {participants.length}</div>
         </div>
     );
 };
