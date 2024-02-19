6f1b5463441fcbf98b2bdc854660a1d88ce07a2f
Noel Ohaeri <noelohaeri@gmail.com>
2024-02-19 18:35:08 -0500

    Update SearchNav styling

--- app/components/search/SearchNav.tsx
+++ app/components/search/SearchNav.tsx
@@ -7,20 +7,20 @@ import Link from 'next/link';
 const SearchNav = () => {
     const path = usePathname();
     return (
-        <div className='flex justify-center my-2 w-screen'>
+        <div className='flex justify-center my-2 md:my-1 lg:my-0 w-screen'>
             <div className='flex justify-between gap-2'>
                 <Link href='/1on1'>
-                    <Button className='rounded-3xl transition-all' size='sm' variant={path === '/1on1' ? 'destructive' : 'googleButton'}>
+                    <Button className='rounded-3xl transition-all' size='sm' variant={path === '/1on1' ? 'destructive' : 'searchNav'}>
                         1 on 1
                     </Button>
                 </Link>
                 <Link href='/'>
-                    <Button className='rounded-3xl transition-all' size='sm' variant={path === '/' ? 'destructive' : 'googleButton'}>
+                    <Button className='rounded-3xl transition-all' size='sm' variant={path === '/' ? 'destructive' : 'searchNav'}>
                         Live
                     </Button>
                 </Link>
                 <Link href='/discover'>
-                    <Button className='rounded-3xl transition-all' size='sm' variant={path === '/discover' ? 'destructive' : 'googleButton'}>
+                    <Button className='rounded-3xl transition-all' size='sm' variant={path === '/discover' ? 'destructive' : 'searchNav'}>
                         Discover
                     </Button>
                 </Link>
