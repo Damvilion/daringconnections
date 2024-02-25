b0fb98883e02056a1f8d4b826a973524440f9035
Noel Ohaeri <noelohaeri@gmail.com>
2024-02-25 17:02:03 -0500

    Update ConnectButton tooltip text

--- app/components/ConnectButton.tsx
+++ app/components/ConnectButton.tsx
@@ -18,7 +18,7 @@ const ConnectButton = () => {
                 <TooltipTrigger
                     onClick={handleClick}
                     className='w-full bg-purple-500 hover:bg-purple-400 text-white h-10 px-4 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'>
-                    Get to know
+                    Get Intimate
                     <TooltipContent side='bottom'>
                         <div className='flex flex-col gap-1'>
                             <span className='text-center text-sm'>Direct message - 1 on 1 video calls & more</span>
